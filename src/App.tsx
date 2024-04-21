7ec72bd6596242f5ff6467082f675ed270ce077b
sdulal123 <sushildulal123@gmail.com>
2024-04-21 19:31:30 +0200

    00_Hangman_directory_setup "primary"

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import { PageHeader } from "./layout/pageHeader";
-import { CategoryTubes } from "./component/categoryTubes";
-import { categories } from "./homepageRecom/topBarRecom";
 import { useState } from "react";
+import words from "./englishWords.json";
 
-export default function App() {
+function App() {
+  const [wordToGuess, setWordToGuess] = useState (() => {
+    return words[Math.floor(Math.random()* words.length)]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
-  const [selectedCategory, setSelectedCategory] = useState(categories[0])
-
+  console.log(wordToGuess)
   return (
-    <div className="max-h-screen flex flex-col">
-      <PageHeader />
-      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
-        <div>Sidebar</div>
-        <div className="overflow-x-hidden px-8 pb-4">
-        <div className="sticky top-0 bg-white z-10 pb-4">
-          <CategoryTubes 
-            categories={categories} 
-            selectedCategory={selectedCategory}
-            onSelect={setSelectedCategory}/>
-        </div>
-        </div>
-      </div>
-    </div>
-  )  
+  <h1>Hi</h1>
+)
 }
+
+export default App
