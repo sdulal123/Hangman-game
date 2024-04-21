b2f5f852cc1e9c8645253a496c37964537a10646
sdulal123 <sushildulal123@gmail.com>
2024-04-21 18:23:13 +0200

    Changes

---
+++ src/homepageRecom/topBarRecom.tsx
@@ -0,0 +1,23 @@
+export const categories = [
+    "All",
+    "React",
+    "Gaming",
+    "Coding",
+    "Next.js",
+    "Cat memes",
+    "Dog videos",
+    "LoFi Music",
+    "Programming",
+    "Procreate Art",
+    "Classical music",
+    "Game Development",
+    "Impractical Jokes",
+    "Hollywood movie trailor",
+    "Blender 3D art challenges",
+]
+
+export const videos = [
+    {
+        
+    }
+]
